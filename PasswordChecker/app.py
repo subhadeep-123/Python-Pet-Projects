7cf287754e4eba6deaf93066f747de00d9e70710
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-08-09 20:43:41 +0530

    Updated

---
+++ PasswordChecker/app.py
@@ -0,0 +1,40 @@
+import requests
+import hashlib
+import sys
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(
+            f'Error Fetching {res.status_code}, check the api and try again')
+    return res
+
+
+def get_pass_counts(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    head, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(head)
+    return get_pass_counts(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found {count} time you should probably change your password')
+        else:
+            print(f'{password} was not found CARRY ON!!')
+
+
+main(sys.argv[1:])
